175fb0d7527b39ee3723cd9a6f3d4f8c34663653
vitorfigueredo <vmsfigueredo@gmail.com>
2022-04-18 23:33:40 -0300

    Assignment Requirements Adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 const lastUpdated = document.querySelector('#lastUpdated');
 lastUpdated.textContent = `Last Updated: ${document.lastModified}`
+
+const footerYear = document.querySelector('#footerYear');
+footerYear.textContent = new Date().getFullYear();
