d5bcfc852819174d215e0ad94f0155e394564399
vitorfigueredo <vmsfigueredo@gmail.com>
2022-05-25 17:12:06 -0300

    Weather Home Page Update

---
+++ weather/js/index.js
@@ -0,0 +1,33 @@
+var request = new XMLHttpRequest();
+
+// Open a new Connection and get Data
+request.open('GET', 'https://byui-cit230.github.io/weather/data/towndata.json', true);
+
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        const data = JSON.parse(this.response);
+        data.towns.map((town) => {
+            town.id = town.name.toLowerCase().replace(" ", "");
+            console.log(town);
+            const year = document.querySelector(`.${town.id}-year`);
+            const pop = document.querySelector(`.${town.id}-pop`);
+            const rain = document.querySelector(`.${town.id}-rain`);
+
+            if (year !== null){
+                year.textContent = town.yearFounded;
+            }
+            if (pop !== null){
+                pop.textContent = town.currentPopulation;
+            }
+            if (rain !== null){
+                rain.textContent = town.averageRainfall;
+            }
+        
+        })
+    } else {
+
+    }
+}
+// Send Request 
+
+request.send();
